0d5edde2c296b6f2e69d6cbbdd675b612cfa32fb
Eric Honorez <eric.honorez@gmail.com>
2016-11-23 21:40:43 +0100

    Changed: date formatted with yyyy-MM-dd

--- web/app/views/helpers/AccountHelper.scala
+++ web/app/views/helpers/AccountHelper.scala
@@ -1,5 +1,8 @@
 package views.helpers
 
+import java.text.SimpleDateFormat
+import java.util.Date
+
 object AccountHelper {
 
   import views.html.helper.FieldConstructor
@@ -7,6 +10,11 @@ object AccountHelper {
 
   def transactionAmountClassOf(value: BigDecimal): String = {
     if (value.compare(BigDecimal.valueOf(0)) < 0) "debit" else "credit"
+  }
+
+  def formatDate(date: Date): String = {
+    val format: SimpleDateFormat = new SimpleDateFormat("yyyy-MM-dd")
+    format.format(date)
   }
 
 }
